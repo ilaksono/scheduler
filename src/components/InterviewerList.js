8b46cd1a8fc1429d07d3791b494c1a6c4c7cac96
ilaksono <ian.laksono@gmail.com>
2020-10-26 18:57:50 -0400

    add int list component

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import classNames from 'classnames';
+import './InterviewerListItem.scss';
+import './InterviewerList.scss';
+import InterviewerListItem from './InterviewerListItem';
+
+
+
+export default function InterviewerList(props) {
+    const ints = props.interviewers;
+    const intsParsed = ints.map((inter) => {
+      return(
+        <InterviewerListItem 
+        key={inter.id} 
+        name={inter.name} 
+        avatar={inter.avatar}
+        setInterviewer={(event) => props.setInterviewer(inter.id)}
+        selected={inter.id === props.interviewer}/>
+      )
+    })
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{intsParsed}</ul>
+    </section>
+  );
+}
---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import classNames from 'classnames';
+import './InterviewerListItem.scss'
+export default function InterviewerListItem(props) {
+  const liClassName = classNames('interviewers__item',{'interviewers__item--selected':props.selected});
+  return (
+    <li className={liClassName} onClick={props.setInterviewer}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt="Sylvia Palmer"
+      />
+      {props.selected && props.name}
+    </li>
+  );
+}
