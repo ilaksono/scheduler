19bf640582d63d0a7174a0f6a216dca9405785a9
ilaksono <ian.laksono@gmail.com>
2020-11-08 15:45:35 -0500

    merge api

---
+++ scheduler-api/src/index.js
@@ -0,0 +1,39 @@
+const PORT = process.env.PORT || 8001;
+const ENV = require("./environment");
+
+const app = require("./application")(ENV, { updateAppointment });
+const server = require("http").Server(app);
+
+const WebSocket = require("ws");
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", socket => {
+  socket.onmessage = event => {
+    console.log(`Message Received: ${event.data}`);
+    if (event.data === "ping") {
+      socket.send(JSON.stringify("pong"));
+    }
+  };
+  socket.on('close', function close() {
+    console.log('it died');
+  });
+});
+
+
+function updateAppointment(id, interview) {
+  wss.clients.forEach(function eachClient(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(
+        JSON.stringify({
+          type: "SET_INTERVIEW",
+          id,
+          interview
+        })
+      );
+    }
+  });
+}
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT} in ${ENV} mode.`);
+});
